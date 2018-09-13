8c9da27390430e3438200973650e03c32d1e485b
Sumit AGrawal <sumit24jun@yahoo.co.in>
2018-09-13 20:08:28 +0530

    added new commit by sumit

--- testing.java
+++ testing.java
@@ -2,7 +2,7 @@ public class Test{
 	public static void neha() {
 		System.out.println("this is neha output1");
 	}
-}public class Test{
+}public class Test addedbySumit{
 	public static void sumit() {
 		System.out.println("this is neha output1");
 	}
